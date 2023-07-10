a5bd8905db3ded60b0fa2baddbe5fd479c9e92ce
ReginaKost <15-15@mail.ru>
2023-07-10 19:04:29 +0300

    update

--- newapp/views.py
+++ newapp/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.views.generic import ListView, DetailView  # импортируем класс получения деталей объекта
+from newapp.models import Post
+from datetime import datetime
 
-# Create your views here.
+
+class PostList(ListView):
+    model = Post
+    template_name = 'news.html'
+    context_object_name = 'post'
+    queryset = Post.objects.order_by('-id')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow() # добавим переменную текущей даты time_now
+        context['value1'] = None # добавим ещё одну пустую переменную, чтобы на её примере посмотреть работу другого фильтра
+        return context
+
+# создаём представление, в котором будут детали конкретного отдельного товара
+class PostDetail(DetailView):
+    model = Post  # модель всё та же, но мы хотим получать детали конкретно отдельного товара
+    template_name = 'news.html'  # название шаблона будет product.html
+    context_object_name = 'post'  # название объекта
