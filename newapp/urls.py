c6a7c4333b7a7f1826b03adf6f5c979fb8639f56
ReginaKost <15-15@mail.ru>
2023-07-20 14:50:40 +0300

    d6

--- newapp/urls.py
+++ newapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import PostList, PostDetail, PostSearch, PostCreate, PostUpdate, PostDelete
+from django.views.decorators.cache import cache_page
 
 app_name = 'newapp'
 urlpatterns = [
@@ -9,4 +10,10 @@ urlpatterns = [
     path('<int:pk>/delete/', PostDelete.as_view(), name='post_delete'),
     path('search/', PostSearch.as_view(), name='search'),
     path('add/', PostCreate.as_view(), name='post_create'),
+    path('category/<int:pk>/', NewsCategoryView.as_view(), name='categories'),
+    path('subscribe/<int:pk>/', subscribe_to_category, name='subscribe'),
+    path('unsubscribe/<int:pk>/', unsubscribe_to_category, name='unsubscribe'),
+    path('<int:pk>/', cache_page(60*10),
+
+
 ]
